ddef32f0ebedaba5055d92fcffc25ba7bc002e69
Arckanna <valerie.lecoeur75@gmail.com>
2018-08-13 13:59:51 +0200

    dans le js gestion du nombre essai et comparaison proposition solution

--- devinette.js
+++ devinette.js
@@ -1,16 +1,33 @@
 alert("Bienvenue dans ce jeu de devinette !");
 var solution, proposition;
- 
+var essai=0;
 
-//fonction aleatoire
-function aleatoire() {
-    // Cette ligne génère aléatoirement un nombre entre 1 et 100
-    solution = Math.floor(Math.random() * 100) + 1;
+
+
+// Cette ligne génère aléatoirement un nombre entre 1 et 100
+solution = Math.floor(Math.random() * 100) + 1;
+
+
+
+proposition = Number(prompt("Devinez un nombre entre 1 et 100 vous avez 6 essais :"));
+
+// recherche de solution
+while ((proposition !== solution) && (essai < 6)) {
+
+    alert("essai :" + essai);
+    essai++;
+    if (proposition < solution) {
+        alert("Plus grand");
+    } else {
+        alert("Plus petit");
+    }
+    proposition = Number(prompt("essayer encore :"));
 }
 
-//debut
-function debut() {
-    proposition = Number(prompt("Devinez un nombre entre 1 et 100 vous avez 6 essais :"));
-}
 
-debut();
+// resultat
+if (proposition == solution) {
+    alert("Bravo vous avez trouver la solution !");
+} else {
+    alert("Raté la solution etait " + solution + " une prochaine fois peut-etre");
+}
